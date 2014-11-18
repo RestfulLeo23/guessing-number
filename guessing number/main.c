7e80a3bf1059190b2cccc624b9abca45edafc185
Scott English <bonnieline@Bonnie-Lines-MacBook-Air-4.local>
2014-11-18 13:27:39 -0600

    Initial Commit

---
+++ guessing number/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  guessing number
+//
+//  Created by Scott English on 01/6/2012.
+//  Copyright (c) 2014 Scott English. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
